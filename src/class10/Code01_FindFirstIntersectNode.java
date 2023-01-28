d104bddbb17323f4f238c47f0ac18690629b590a
limiao5 <limiao5@xiaomi.com>
2023-01-28 19:51:54 +0800

    第11节-链表-链表相交问题

--- src/class10/Code01_FindFirstIntersectNode.java
+++ src/class10/Code01_FindFirstIntersectNode.java
@@ -1,5 +1,6 @@
 package class10;
 
+// 链表相交问题
 public class Code01_FindFirstIntersectNode {
 
 	public static class Node {
@@ -17,12 +18,13 @@ public class Code01_FindFirstIntersectNode {
 		}
 		Node loop1 = getLoopNode(head1);
 		Node loop2 = getLoopNode(head2);
-		if (loop1 == null && loop2 == null) {
+		if (loop1 == null && loop2 == null) { // 都无环
 			return noLoop(head1, head2);
 		}
-		if (loop1 != null && loop2 != null) {
+		if (loop1 != null && loop2 != null) { // 都有环
 			return bothLoop(head1, loop1, head2, loop2);
 		}
+		// 一个链表有环，一个链表无环，一定不会相交
 		return null;
 	}
 
@@ -58,22 +60,22 @@ public class Code01_FindFirstIntersectNode {
 		Node cur1 = head1;
 		Node cur2 = head2;
 		int n = 0;
-		while (cur1.next != null) {
+		while (cur1.next != null) { // 走到end节点
 			n++;
 			cur1 = cur1.next;
 		}
-		while (cur2.next != null) {
+		while (cur2.next != null) { // 走到end节点
 			n--;
 			cur2 = cur2.next;
 		}
-		if (cur1 != cur2) {
+		if (cur1 != cur2) { // 两个链表的end节点不相等，那么一定不会相交
 			return null;
 		}
 		// n  :  链表1长度减去链表2长度的值
 		cur1 = n > 0 ? head1 : head2; // 谁长，谁的头变成cur1
 		cur2 = cur1 == head1 ? head2 : head1; // 谁短，谁的头变成cur2
 		n = Math.abs(n);
-		while (n != 0) {
+		while (n != 0) { // 长的链表提前走
 			n--;
 			cur1 = cur1.next;
 		}
@@ -88,7 +90,7 @@ public class Code01_FindFirstIntersectNode {
 	public static Node bothLoop(Node head1, Node loop1, Node head2, Node loop2) {
 		Node cur1 = null;
 		Node cur2 = null;
-		if (loop1 == loop2) {
+		if (loop1 == loop2) { // 相交在同一个节点
 			cur1 = head1;
 			cur2 = head2;
 			int n = 0;
@@ -112,10 +114,10 @@ public class Code01_FindFirstIntersectNode {
 				cur2 = cur2.next;
 			}
 			return cur1;
-		} else {
+		} else { // 相交在不同的节点 + 两个环不相交
 			cur1 = loop1.next;
 			while (cur1 != loop1) {
-				if (cur1 == loop2) {
+				if (cur1 == loop2) { //  相交在不同的节点
 					return loop1;
 				}
 				cur1 = cur1.next;
